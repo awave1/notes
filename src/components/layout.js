67c971e6e6e33d14a6cd38249212b5e23a9a358f
awave1 <artgolovin96@gmail.com>
2018-12-21 16:28:31 -0700

    refactor components to use styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+import Navbar from './navbar';
 
-import Header from './header';
 import './layout.css';
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+  padding-top: 0;
+`;
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,17 +26,8 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <Navbar siteTitle={data.site.siteMetadata.title} />
+        <Container>{children}</Container>
       </>
     )}
   />
