c82ac1c591a61b41ebcbf131bfeabb3563e2d4f0
awave1 <artgolovin96@gmail.com>
2018-12-25 13:11:22 -0700

    implement list of posts

---
+++ src/components/postLink.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default ({ post }) => (
+  <div>
+    <Link to={post.frontmatter.path}>
+      {post.frontmatter.title} ({post.frontmatter.date})
+    </Link>
+  </div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
-
 import Layout from '../components/layout';
+import PostLink from '../components/postLink';
 
-export default () => (
-  <Layout>
-    <h1>ok so basically</h1>
-  </Layout>
-);
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date)
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
+
+  return (
+    <Layout>
+      <div>{Posts}</div>
+    </Layout>
+  );
+};
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
