2bc05b62ebf2276d98cc3cc5254c49c5240b3309
Artem Golovin <artem.golovin@renoworks.com>
2018-12-26 12:54:08 -0700

    upd config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@ module.exports = {
     title: 'yea',
     description:
       'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    author: '@awaveawave',
+    siteUrl: 'https://awave1.github.io/notes',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
