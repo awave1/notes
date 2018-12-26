d2fdfd1c745a4cb65d89010c1821030fa93b29a3
awave1 <artgolovin96@gmail.com>
2018-12-26 14:14:37 -0700

    update metadata & package.json info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   pathPrefix: '/notes',
   siteMetadata: {
-    title: 'yea',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    title: 'notes',
+    description: 'Collection of my university notes.',
     author: '@awaveawave',
     siteUrl: 'https://awave1.github.io/notes',
   },
